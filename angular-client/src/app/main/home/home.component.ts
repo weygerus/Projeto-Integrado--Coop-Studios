076b22d19fefdcb2f373c7f93c31c1325a58913c
weygerus <gabrileao38@gmail.com>
2022-05-11 07:48:27 -0300

    11/05/2022 - 1

--- angular-client/src/app/main/home/home.component.ts
+++ angular-client/src/app/main/home/home.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as $ from 'jquery';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./banner.component.css','searchNav.component.css','./highligth.component.css']
+  styleUrls: [
+    './css/banner.component.css',
+    './css/searchNav.component.css',
+    './css/highligth.component.css',
+  ]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+
+
+   }
 
   ngOnInit(): void {
+
+
+    $('.highlight__list__single').on('click', function(){
+      if (!$(this).hasClass('active')) {
+          var dataIndex = $(this).attr('data-index');
+          $('.highlight__list__single.active').removeClass('active');
+          $('.highlight__current.active').removeClass('active');
+          $(this).addClass('active');
+          $('.'+dataIndex).addClass('active');
+      }
+  });
+  $('.filterItens__single').addClass('active');
+  $('.filterItens__categories').on('change', function() {
+      var optionValue = $('.filterItens__categories option:selected').val();
+      $('.filterItens__single.active').removeClass('active');
+      $('.'+optionValue).addClass('active');
+  })
+
   }
 
 }
