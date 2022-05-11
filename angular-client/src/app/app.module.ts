cdd15ec9186045c0611c784c3d5aefa211efcb7d
weygerus <gabrileao38@gmail.com>
2022-05-10 21:13:51 -0300

    10/05/2022 - 3

---
+++ angular-client/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './shared/nav/nav.component';
+import { AuthComponent } from './core/auth/auth.component';
+import { HomeComponent } from './main/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    AuthComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angular-client/src/app/main/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./banner.component.css','searchNav.component.css','./highligth.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
