96518a36e331907ca71d29803752d33b627b2115
weygerus <gabrileao38@gmail.com>
2022-05-17 13:45:49 -0300

    17/05/2022-2

--- angular-client/src/app/shared/nav/nav.component.ts
+++ angular-client/src/app/shared/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as $ from 'jquery';
+
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
@@ -10,6 +12,32 @@ export class NavComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    $(function() {
+
+      const main_header = document.getElementById('main-header');
+      const main_nav = document.getElementById('main-nav');
+      const main_li = document.getElementById('ul')
+
+        $(window).on("scroll", function() {
+          if($(window).scrollTop()! < 50) {
+            main_header!.style.display = 'block';
+            main_header!.style.backgroundColor = 'transparent';
+            main_nav!.style.paddingTop = '25px';
+            main_li!.style.color = '#ffff';
+          }
+          else if($(window).scrollTop()! > 50 && $(window).scrollTop()! < 550 ) {
+            main_header!.style.display = 'block';
+            main_header!.style.backgroundColor = '#fafafa';
+            main_nav!.style.paddingTop = '10px';
+            main_li!.style.color = '#101010';
+          }
+          else {
+            main_header!.style.display = 'none'
+          }
+
+          
+        })
+    });
   }
 
 }
--- angular-client/src/app/shared/search-nav/search-nav.component.ts
+++ angular-client/src/app/shared/search-nav/search-nav.component.ts
@@ -10,6 +10,21 @@ export class SearchNavComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    $(function() {
+
+      const search_header = document.getElementById('search-header');
+      const search_nav = document.getElementById('search-nav');
+      const search_li = document.getElementById('ul');
+
+        $(window).on("scroll", function() {
+          if($(window).scrollTop()! > 550) {
+            search_header!.style.display = 'block';
+          }
+          else {
+            search_header!.style.display = 'none'
+          }
+        })
+    });
   }
 
 }
